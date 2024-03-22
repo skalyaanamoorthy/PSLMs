5db80a454a444fa0a8a019967f91ef3260d850ba
Subha Kalyaanamoorthy <64178961+skalyaanamoorthy@users.noreply.github.com>
2024-03-21 21:38:30 -0400

    Update append_modeller_paths.sh
    Updated for new modeller version

--- convenience_scripts/append_modeller_paths.sh
+++ convenience_scripts/append_modeller_paths.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-cat <<EOT >> ./pslm/bin/activate
+cat <<EOT >> /opt/venv/bin/activate
 
 if [ -z \$PYTHONPATH ]
 then
-    export PYTHONPATH="/usr/lib/modeller10.4/modlib/:/usr/lib/modeller10.4/lib/x86_64-intel8/python3.3/"
+    export PYTHONPATH="/usr/lib/modeller10.5/modlib/:/usr/lib/modeller10.5/lib/x86_64-intel8/python3.3/"
 else
-    export PYTHONPATH="/usr/lib/modeller10.4/modlib/:/usr/lib/modeller10.4/lib/x86_64-intel8/python3.3/:\${PYTHONPATH}:"
+    export PYTHONPATH="/usr/lib/modeller10.5/modlib/:/usr/lib/modeller10.5/lib/x86_64-intel8/python3.3/:\${PYTHONPATH}:"
 fi
 
 if [ -z \$LD_LIBRARY_PATH ]
 then
-    export LD_LIBRARY_PATH="/usr/lib/modeller10.4/lib/x86_64-intel8"
+    export LD_LIBRARY_PATH="/usr/lib/modeller10.5/lib/x86_64-intel8"
 else
-    export LD_LIBRARY_PATH="\${LD_LIBRARY_PATH}:/usr/lib/modeller10.4/lib/x86_64-intel8"
+    export LD_LIBRARY_PATH="\${LD_LIBRARY_PATH}:/usr/lib/modeller10.5/lib/x86_64-intel8"
 fi
 EOT
 
