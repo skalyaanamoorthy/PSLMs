393fe63f425118a3d823aa5dc6a1d721a3b1fad7
Shawn Reeves <sareeves@uwaterloo.ca>
2024-05-02 22:15:09 -0400

    Updates to inference scripts and Docker

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ FROM pytorch/pytorch:latest
 # Set the working directory inside the container
 WORKDIR /app
 
+# Set a non-interactive frontend (this prevents interactive prompts during package installation)
+ENV DEBIAN_FRONTEND noninteractive
+
 # Install any necessary dependencies (if required by PyTorch)
 RUN apt-get update && apt-get install -y \
     build-essential \
@@ -13,12 +16,20 @@ RUN apt-get update && apt-get install -y \
     git \
     wget \
     unzip \
+    vim \
     git-lfs \
     libglib2.0-0 \
+    ca-certificates \
+    && update-ca-certificates \
     && rm -rf /var/lib/apt/lists/*
 
+# Reset the frontend to its default mode
+ENV DEBIAN_FRONTEND newt
 ENV NVIDIA_VISIBLE_DEVICES all
 ENV NVIDIA_DRIVER_CAPABILITIES compute,utility
+ENV REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+ENV SSL_CERT_DIR=/etc/ssl/certs
+ENV SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
 
 # Assuming that the current directory (.) contains the PSLM repository contents
 # and that it is being copied into the image at /app
@@ -31,7 +42,8 @@ COPY . /app
 # to get large files / datasets
 RUN git lfs install --skip-smudge && \
     git lfs pull && \
-    unzip ./data/preprocessing/msas.zip -d ./data/preprocessing/msas && \
+    unzip ./data/preprocessed/msas.zip -d ./data/preprocessed/msas && \
+    unzip ./data/preprocessed/weights.zip -d ./data/preprocessed/weights && \
     unzip ./data/rosetta_predictions.zip -d ./data/rosetta_predictions
 
 # Create a virtual environment
@@ -47,7 +59,9 @@ RUN . /opt/venv/bin/activate && \
 # OPTIONAL SECTION: For download prediction tools which are not Python packages
 # COMMENT OUT tools you don't need or that break your installation
 # Note that these have not been tested on other systems
-RUN git clone https://github.com/thomaskf/AliStat
+RUN git clone https://github.com/thomaskf/AliStat && \
+    cd AliStat && \
+    make
 
 RUN git clone https://github.com/dauparas/ProteinMPNN
 
@@ -70,6 +84,8 @@ RUN chmod +x convenience_scripts/append_modeller_paths.sh && \
 ### NOTE: YOU MAY HAVE TO MODIFY THE SCRIPT ABOVE FOR YOUR SYSTEM ###
 
 # END OPTIONAL SECTION
+
+RUN chmod -R 777 /app
 
 #RUN #. /opt/venv/bin/activate && \
 #    python preprocessing/preprocess.py --dataset q3421
--- inference_scripts/ankh_.py
+++ inference_scripts/ankh_.py
@@ -13,7 +13,21 @@ def score_sequences(args):
     df2 = df.groupby('uid').first()
     dataset = args.dataset
 
-    model, tokenizer = ankh.load_large_model(generation=True)
+    if args.model == 'ankh_large':
+        if args.mode in ['decoder_only', 'both', 'transformer']:
+            model, tokenizer = ankh.load_large_model(generation=True)
+        elif args.mode in ['encoder_only']:
+            model, tokenizer = ankh.load_large_model(generation=False)
+        else:
+            assert False
+        args.model == 'ankh'
+    elif args.model == 'ankh_base':
+        if args.mode in ['decoder_only', 'both', 'transformer']:
+            model, tokenizer = ankh.load_base_model(generation=True)
+        elif args.mode in ['encoder_only']:
+            model, tokenizer = ankh.load_base_model(generation=False)
+        else:
+            assert False       
     amino_acids = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
     amino_acid_ids = [tokenizer.convert_tokens_to_ids(aa) for aa in amino_acids]
     model.eval()
@@ -24,13 +38,15 @@ def score_sequences(args):
     else:
         device='cpu'
 
+    mode = args.mode
     logps = pd.DataFrame(index=df2.index,columns=[f'ankh_dir'] + [f'runtime_ankh_dir'])
    
     with tqdm(total=len(df2)) as pbar:
         for code, group in df2.groupby('code'):
             for uid, row in group.iterrows():
                 with torch.no_grad():
-                    try:
+                    #try:
+                    if True:
                         pos = row['position']
                         wt = row['wild_type']
                         mt = row['mutation']
@@ -42,23 +58,33 @@ def score_sequences(args):
                         oc = int(ou) * (0 if dataset == 'fireprot' else -1)  -1 #-ws
                         idx = pos + oc
 
+                        # Convert sequence to list and insert mask token
                         sequence = list(sequence)
                         sequence[idx] = '<extra_id_0>'
                         masked_seq = ''.join(sequence)
-                        
-                        #print(wt, pos, mt, pos+oc)
-                        #print(masked_seq)
+
                         start = time.time()
-
                         encoded = tokenizer.encode_plus(masked_seq, add_special_tokens=True, return_tensors='pt')
-
                         input_ids = encoded['input_ids'].to(device)
                         attention_mask = encoded['attention_mask'].to(device)
-                        decoder_input_ids = input_ids.clone().to(device)
 
                         with torch.no_grad():
-                            outputs = model(input_ids=input_ids, attention_mask=attention_mask, decoder_input_ids=decoder_input_ids)
-                            logits = outputs.logits
+                            if mode == 'encoder_only':
+                                # For encoder-only mode, do not pass decoder_input_ids
+                                outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+                                logits = outputs.last_hidden_state
+                            elif mode == 'decoder_only' or mode in ['both', 'transformer']:
+                                # Use the full encoder-decoder model
+                                if mode == 'decoder_only':
+                                    # Initialize decoder_input_ids with pad tokens and then replace the first token
+                                    decoder_input_ids = torch.full_like(input_ids, tokenizer.pad_token_id)
+                                    decoder_input_ids[:, 0] = tokenizer.mask_token_id
+                                else:
+                                    # For both mode, decoder_input_ids mirror input_ids
+                                    decoder_input_ids = input_ids.clone()
+
+                                outputs = model(input_ids=input_ids, attention_mask=attention_mask, decoder_input_ids=decoder_input_ids)
+                                logits = outputs.logits
 
                         # Extract logits for the masked position
                         position = (input_ids[0] == tokenizer.convert_tokens_to_ids('<extra_id_0>')).nonzero().item()
@@ -68,20 +94,22 @@ def score_sequences(args):
                         masked_position_probabilities = torch.log_softmax(masked_position_logits, dim=0)[amino_acid_ids]
 
                         score = masked_position_probabilities[amino_acids.index(mt)] - masked_position_probabilities[amino_acids.index(wt)]
-                        logps.at[uid, f'ankh_dir'] = score.cpu().item()
-                        logps.at[uid, f'runtime_ankh_dir'] = time.time() - start
+                        logps.at[uid, f'{args.model}_{args.mode}_dir'] = score.cpu().item()
+                        logps.at[uid, f'runtime_{args.model}_{args.mode}_dir'] = time.time() - start
+
                         
-                    except Exception as e:
-                        print(code, wt, pos, mt, e)
-                        logps.at[uid, f'ankh_dir'] = np.nan
-                        logps.at[uid, f'runtime_ankh_dir'] = np.nan
+                    #except Exception as e:
+                    #    print(code, wt, pos, mt, e)
+                    #    logps.at[uid, f'ankh_{args.mode}_dir'] = np.nan
+                    #    logps.at[uid, f'runtime_ankh_{args.mode}_dir'] = np.nan
                     pbar.update(1)
 
     df = pd.read_csv(args.output)
     df = df.set_index('uid')
     logps.index.name = 'uid'
-    df = df.drop([col for col in df.columns if 'ankh' in col], axis=1)
+    df = df.drop([col for col in df.columns if f'{args.model}_{args.mode}' in col], axis=1)
     df = df.join(logps)
+    print(df)
     df.to_csv(args.output)
 
 
@@ -103,6 +131,15 @@ def main():
             '--inverse', action='store_true', default=False,
             help='use the mutant structure and apply a reversion mutation'
     )
+    parser.add_argument(
+            '--mode', default='both',
+            help='one of encoder_only, decoder_only, or both'
+    )
+    parser.add_argument(
+            '--model', default='ankh_large',
+            help='one of ankh_large, ankh_base'
+    )
+
     args = parser.parse_args()
 
     if 'fireprot' in args.db_loc.lower():
